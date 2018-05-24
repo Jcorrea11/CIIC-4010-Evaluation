a23e6d57d5363fad469a2f17b1b5eafcadc748a5
Jose Correa <jose.correa11@upr.edu>
2018-05-24 11:51:16 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int functionality, design, teamwork, efficiency, readable, quality;
+		float percentage;
+		Scanner inputNumScanner = new Scanner(System.in);
+	}
+
+}
